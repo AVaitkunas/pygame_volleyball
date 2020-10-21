f24cc9712cc827311b9822855505a85ba70c9e73
AVaitkunas <39170257+AVaitkunas@users.noreply.github.com>
2020-10-21 09:09:17 +0300

    added comments for future me

--- volleyball.py
+++ volleyball.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+# SETTINGS
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
@@ -19,29 +20,32 @@ class Ball:
         self.gravity = 1
         self.is_stopped = False
         ball_image = pygame.image.load("ball.png")
-        self.ball = pygame.transform.scale(ball_image, (diameter,) * 2)
-        self.ball_rect = self.ball.get_rect()
+        self.image = pygame.transform.scale(ball_image, (diameter,) * 2)
+        self.ball_rect = self.image.get_rect()
         self.elasticity = 0.8
 
     def move(self):
-
+        """Makes ball move. Gravity is taken into account."""
         if not self.is_stopped:
             self.speed_y += self.gravity
+            # handling of ball appearing bellow screen boundaries
             if self.ball_rect.bottom + self.speed_y < SCREEN_HEIGHT:
                 self.ball_rect = self.ball_rect.move(self.speed_x, self.speed_y)
             else:
                 self.ball_rect = self.ball_rect.move(self.speed_x, SCREEN_HEIGHT - self.ball_rect.bottom)
 
+            # handling of ball bounces from walls
             if self.ball_rect.right >= SCREEN_WIDTH or self.ball_rect.left < 0:
                 self.speed_x *= -self.elasticity
             if self.ball_rect.bottom >= SCREEN_HEIGHT or self.ball_rect.top < 0:
                 self.speed_y *= -self.elasticity
 
+            # makes ball stop if not enough energy left for ball
             if self.ball_rect.bottom == SCREEN_HEIGHT and self.speed_y > -5:
                 self.is_stopped = True
 
     def show(self, surface):
-        surface.blit(self.ball, self.ball_rect)
+        surface.blit(self.image, self.ball_rect)
 
 
 pygame.init()
